a2b0b144dd16f8e2f80d33f0ff9fe4fe9ce77841
Les Aker <me@lesaker.org>
2018-03-03 21:18:15 -0500

    update with routes

--- main.go
+++ main.go
@@ -2,20 +2,47 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/akerl/go-lambda/apigw/events"
+	"github.com/akerl/go-lambda/apigw/router"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Hello world")
+var debugRegex = regexp.MustCompile(`^/debug$`)
+var authRegex = regexp.MustCompile(`^/auth$`)
+var callbackRegex = regexp.MustCompile(`^/callback$`)
+var indexRegex = regexp.MustCompile(`^/`)
+var defaultRegex = regexp.MustCompile(`.*`)
 
-	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("%+v\n", request),
-		StatusCode: 200,
-	}, nil
+func debugHandler(req events.Request) (events.Response, error) {
+	return events.Succeed(fmt.Sprintf("%+v\n", req))
+}
+
+func authHandler(req events.Request) (events.Response, error) {
+	return events.Succeed("Auth!")
+}
+
+func callbackHandler(req events.Request) (events.Response, error) {
+	return events.Succeed("Callback!")
+}
+
+func indexHandler(req events.Request) (events.Response, error) {
+	return events.Succeed("Index!")
+}
+
+func defaultHandler(req events.Request) (events.Response, error) {
+	return events.Succeed(fmt.Sprintf("Catch-all: %s", req.Path))
 }
 
 func main() {
-	lambda.Start(handler)
+	r := router.Router{
+		Routes: []router.Route{
+			router.Route{Path: debugRegex, Handler: debugHandler},
+			router.Route{Path: authRegex, Handler: authHandler},
+			router.Route{Path: callbackRegex, Handler: callbackHandler},
+			router.Route{Path: indexRegex, Handler: indexHandler},
+			router.Route{Path: defaultRegex, Handler: defaultHandler},
+		},
+	}
+	r.Start()
 }
